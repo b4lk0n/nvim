606f49f0b1332ee8117be1e3ced7876cf73681f1
Andrii <b4lk0n@gmail.com>
2026-05-25 16:36:38 +0200

    feat(snacks): tune smooth scroll animation

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -1,75 +1,86 @@
 return {
-  "folke/snacks.nvim",
-  priority = 1000,
-  lazy = false,
-  opts = {
-    indent = { enabled = true },
-    scope = { enabled = true },
-    scroll = { enabled = true },
-    dim = { enabled = true },
-    notifier = { enabled = true },
-    notify = { enabled = true },
-    picker = {},
-    statuscolumn = { enabled = true },
-    terminal = { enabled = true },
-    words = { enabled = true },
-  },
-  keys = {
-    {
-      "<leader>sf",
-      function()
-        Snacks.picker.files({
-          hidden = true,
-          ignored = true,
-          follow = true,
-          exclude = {
-            "node_modules",
-            ".next",
-            ".turbo",
-            ".react-router",
-            ".expo",
-            ".git",
-            ".DS_Store",
-            "dist"
-          }
-        })
-      end
-    },
-    {
-      "<leader>sg",
-      function()
-        Snacks.picker.grep()
-      end
-    },
-    {
-      "<leader>sd",
-      function()
-        Snacks.picker.diagnostics()
-      end
-    },
-    {
-      "gd",
-      function()
-        Snacks.picker.lsp_definitions()
-      end
-    },
-    {
-      "grr",
-      function()
-        Snacks.picker.lsp_references()
-      end
-    },
-    {
-      "gO",
-      function()
-        Snacks.picker.lsp_symbols()
-      end
-    },
-    {
-      "<leader>sk",
-      function()
-        Snacks.picker.keymaps()
-      end
-    }
-  }
+	"folke/snacks.nvim",
+	priority = 1000,
+	lazy = false,
+	opts = {
+		indent = { enabled = true },
+		scope = { enabled = true },
+		scroll = {
+			enabled = true,
+			animate = {
+				duration = { step = 4, total = 90 },
+				easing = "outCubic",
+			},
+			animate_repeat = {
+				delay = 80,
+				duration = { step = 2, total = 35 },
+				easing = "outCubic",
+			},
+		},
+		dim = { enabled = true },
+		notifier = { enabled = true },
+		notify = { enabled = true },
+		picker = {},
+		statuscolumn = { enabled = true },
+		terminal = { enabled = true },
+		words = { enabled = true },
+	},
+	keys = {
+		{
+			"<leader>sf",
+			function()
+				Snacks.picker.files({
+					hidden = true,
+					ignored = true,
+					follow = true,
+					exclude = {
+						"node_modules",
+						".next",
+						".turbo",
+						".react-router",
+						".expo",
+						".git",
+						".DS_Store",
+						"dist",
+					},
+				})
+			end,
+		},
+		{
+			"<leader>sg",
+			function()
+				Snacks.picker.grep()
+			end,
+		},
+		{
+			"<leader>sd",
+			function()
+				Snacks.picker.diagnostics()
+			end,
+		},
+		{
+			"gd",
+			function()
+				Snacks.picker.lsp_definitions()
+			end,
+		},
+		{
+			"grr",
+			function()
+				Snacks.picker.lsp_references()
+			end,
+		},
+		{
+			"gO",
+			function()
+				Snacks.picker.lsp_symbols()
+			end,
+		},
+		{
+			"<leader>sk",
+			function()
+				Snacks.picker.keymaps()
+			end,
+		},
+	},
 }
